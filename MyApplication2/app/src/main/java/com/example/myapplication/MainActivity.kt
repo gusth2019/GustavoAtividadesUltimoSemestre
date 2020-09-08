ee2a170260a77f4d6f8d89d612d188521b1b444d
Gustavo Henrique Martins Goncalves <ghgoncalves@stefanini.com>
2020-09-08 14:39:48 -0300

    final da prova

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,31 +15,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun simular(componente:View){
+    fun simular(componente: View){
 
-        val valorAp = 190.00
-        val valorEntrada = Entrada.toString().toDouble()
-        val quantidadeDeParcelas = Parcelas.toString().toInt()
+        val clinte = nomeCliente.text.toString()
+        val valorAp = 190.000
+        val valorEntrada = Entrada.text.toString().toDouble()
+        val quantidadeDeParcelas = Parcelas.text.toString().toInt()
         val sobraValor = valorAp - valorEntrada
         val pagarParcelas = sobraValor / quantidadeDeParcelas
-        val valorMinimo = 0.010 * valorAp
-
+        val valorMinimo = 0.10 * valorAp
+        val df = DecimalFormat("0.###")
+        val valorDasParcelas = df.format(pagarParcelas)
+        
         if (
-            !nomeCliente.toString().all { it.isLetter() } || Entrada.toString().toDouble() < 0 || Entrada.toString().toDouble() > 150.00 ||
-                    Parcelas.toString().toInt() < 0 || Parcelas.toString().toInt() > 235
+            !clinte.all { it.isLetter() } || valorEntrada < 0.0 || valorEntrada > 150.000 ||
+                    quantidadeDeParcelas < 0 || quantidadeDeParcelas > 235
                 ) {
-            Toast.makeText(this, "Algum valor foi digitado incorretamente. Por gentileza, tente novamente", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this,
+                "Algum valor foi digitado incorretamente. Por gentileza, tente novamente",
+                Toast.LENGTH_LONG
+            ).show()
         } else if (
-            valorEntrada > 0 && valorEntrada < valorMinimo
+             valorEntrada  < valorMinimo
                 ) {
             mensagem.text = "Não é possível financiar o empreendimento com esse valor proposto"
             mensagem.setTextColor(Color.RED)
             mensagem.visibility = View.VISIBLE
         } else {
-            mensagem.text = "Com a entrade de ${valorEntrada} e o tempo de financiamento de ${quantidadeDeParcelas} meses, suas parcelas ficarão no valor final de ${pagarParcelas} por mês"
+            mensagem.text = "Com a entrada de ${valorEntrada} e o tempo de financiamento de ${quantidadeDeParcelas} meses, suas parcelas ficarão no valor final de ${valorDasParcelas} por mês"
             mensagem.setTextColor(Color.GREEN)
             mensagem.visibility = View.VISIBLE
         }
-
         }
     }
